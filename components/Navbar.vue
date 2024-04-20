5c17a7ba6b27e12f250fe050653045908928ba88
Jesus Berdugo <berdugopertuz@gmail.com>
2024-04-20 06:59:05 -0500

    chore: create navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+useHead({
+  title: "Home",
+});
+</script>
+
+<template>
+  <header class="flex justify-between items-center">
+    <div>
+      <img src="/public/logo.png" alt="logo" width="100px" />
+    </div>
+
+    <div>
+      <UButton class="bg-transparent hover:bg-transparent text-base font-rubik">Sign In</UButton>
+    </div>
+  </header>
+</template>
